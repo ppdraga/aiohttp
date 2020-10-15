2d69faebc361cf2e7ea17b9a25601de5a37345b7
Yaroslav <ppdraga@gmail.com>
2020-10-15 14:29:15 +0300

    all done

--- aiohttpdemo_polls/db.py
+++ aiohttpdemo_polls/db.py
@@ -28,6 +28,9 @@ choice = Table(
            ForeignKey('question.id', ondelete='CASCADE'))
 )
 
+class RecordNotFound(Exception):
+    """Requested record in database was not found"""
+
 # postgres
 async def init_pg(app):
     conf = app['config']['postgres']
@@ -81,36 +84,55 @@ async def init_sqlite(app):
     engine = await aiosqlite.connect('data.sqlite')
     engine.row_factory = aiosqlite.Row
     app['db'] = engine
+    # yield
 
 async def close_sqlite(app):
     await app['db'].close()
     # await app['db'].wait_closed()
 
 async def get_question_sqlite(conn, question_id):
-    result = await conn.execute(
-        str(question.select()
-        .where(question.c.id == question_id)))
-    question_record = await result.first()
+    result = await conn.execute(str(question.select().where(question.c.id == question_id)), {'id_1': question_id})
+    question_record = await result.fetchone()
     if not question_record:
         msg = "Question with id: {} does not exists"
         raise RecordNotFound(msg.format(question_id))
-    result = await conn.execute(
-        str(choice.select()
-        .where(choice.c.question_id == question_id)
-        .order_by(choice.c.id)))
+    result = await conn.execute(str(choice.select().where(choice.c.question_id == question_id).order_by(choice.c.id)), 
+        {'question_id_1': question_id})
     choice_records = await result.fetchall()
     return question_record, choice_records
 
 
 async def vote_sqlite(conn, question_id, choice_id):
     result = await conn.execute(
-        str(choice.update()
-        .returning(*choice.c)
-        .where(choice.c.question_id == question_id)
-        .where(choice.c.id == choice_id)
-        .values(votes=choice.c.votes+1)))
-    record = await result.fetchone()
-    if not record:
-        msg = "Question with id: {} or choice id: {} does not exists"
-        raise RecordNotFound(msg.format(question_id, choice_id))
+        # str(choice.update()
+        # .returning(*choice.c)
+        # .where(choice.c.question_id == question_id)
+        # .where(choice.c.id == choice_id)
+        # .values(votes=choice.c.votes+1)), 
+        """
+        UPDATE 
+            choice SET votes=(choice.votes + :votes_1) 
+        WHERE 
+            choice.question_id = :question_id_1 AND choice.id = :choice_id 
+        """,
+        {'votes_1': 1, 'question_id_1': question_id, 'choice_id': choice_id, })
+    await conn.commit()
+    # record = await result.fetchone()
+    # if not record:
+    #     msg = "Question with id: {} or choice id: {} does not exists"
+    #     raise RecordNotFound(msg.format(question_id, choice_id))
 
+async def get_question_sqlite(conn, question_id):
+    result = await conn.execute(
+        str(question.select()
+        .where(question.c.id == question_id)), {'id_1': question_id})
+    question_record = await result.fetchone()
+    if not question_record:
+        msg = "Question with id: {} does not exists"
+        raise RecordNotFound(msg.format(question_id))
+    result = await conn.execute(
+        str(choice.select()
+        .where(choice.c.question_id == question_id)
+        .order_by(choice.c.id)), {'question_id_1': question_id})
+    choice_records = await result.fetchall()
+    return question_record, choice_records
--- aiohttpdemo_polls/views.py
+++ aiohttpdemo_polls/views.py
@@ -24,6 +24,43 @@ import db
 #             'choices': choices
 #         }
 
+# @aiohttp_jinja2.template('results.html')
+# async def results(request):
+#     async with request.app['db'].acquire() as conn:
+#         question_id = request.match_info['question_id']
+
+#         try:
+#             question, choices = await db.get_question(conn,
+#                                                       question_id)
+#         except db.RecordNotFound as e:
+#             raise web.HTTPNotFound(text=str(e))
+
+#         return {
+#             'question': question,
+#             'choices': choices
+#         }
+
+# async def vote(request):
+#     async with request.app['db'].acquire() as conn:
+#         question_id = int(request.match_info['question_id'])
+#         data = await request.post()
+#         try:
+#             choice_id = int(data['choice'])
+#         except (KeyError, TypeError, ValueError) as e:
+#             raise web.HTTPBadRequest(
+#                 text='You have not specified choice value') from e
+#         try:
+#             await db.vote(conn, question_id, choice_id)
+#         except db.RecordNotFound as e:
+#             raise web.HTTPNotFound(text=str(e))
+#         router = request.app.router
+#         url = router['results'].url_for(question_id=str(question_id))
+#         return web.HTTPFound(location=url)
+
+
+
+# sqlite:
+
 @aiohttp_jinja2.template('index.html')
 async def index(request):
     async with request.app['db'].execute(str(db.question.select())) as cursor:
@@ -32,16 +69,52 @@ async def index(request):
         # return web.Response(text=str(questions))
         return {"questions": questions}
 
+
 @aiohttp_jinja2.template('detail.html')
 async def poll(request):
-    async with request.app['db'] as conn:
-        question_id = request.match_info['question_id']
-        try:
-            question, choices = await db.get_question_sqlite(conn,
-                                                      question_id)
-        except db.RecordNotFound as e:
-            raise web.HTTPNotFound(text=str(e))
-        return {
-            'question': question,
-            'choices': choices
-        }
+
+    conn = request.app['db']
+    question_id = request.match_info['question_id']
+    try:
+        question, choices = await db.get_question_sqlite(conn,
+                                                    question_id)
+    except db.RecordNotFound as e:
+        raise web.HTTPNotFound(text=str(e))
+    return {
+        'question': question,
+        'choices': choices
+    }
+
+@aiohttp_jinja2.template('results.html')
+async def results(request):
+    conn = request.app['db']
+    question_id = request.match_info['question_id']
+
+    try:
+        question, choices = await db.get_question_sqlite(conn,
+                                                    question_id)
+    except db.RecordNotFound as e:
+        raise web.HTTPNotFound(text=str(e))
+
+    return {
+        'question': question,
+        'choices': choices
+    }
+
+
+async def vote(request):
+    conn = request.app['db']
+    question_id = int(request.match_info['question_id'])
+    data = await request.post()
+    try:
+        choice_id = int(data['choice'])
+    except (KeyError, TypeError, ValueError) as e:
+        raise web.HTTPBadRequest(
+            text='You have not specified choice value') from e
+    try:
+        await db.vote_sqlite(conn, question_id, choice_id)
+    except db.RecordNotFound as e:
+        raise web.HTTPNotFound(text=str(e))
+    router = request.app.router
+    url = router['results'].url_for(question_id=str(question_id))
+    return web.HTTPFound(location=url)
